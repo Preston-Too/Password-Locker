e63501f4adf789a3bc59fcd04fa842bf98f06139
Preston-Too <toopreston@gmail.com>
2020-11-22 19:09:07 +0300

    add method to login and save account

--- user.py
+++ user.py
@@ -128,3 +128,72 @@ class Credentials():
         """
         password = string.ascii_lowercase + string.ascii_uppercase + "~!@#$%;:^&*"
         return ''.join(random.choice(password) for i in range(int(stringLength)))
+
+if __name__ == "__main__":
+    
+    isTrue = True
+    print("Welcome to password Locker.An app to manage and generate passwords.")
+    while isTrue == True:
+        print(
+            "Use these short code to proceed:\n\n 1. cc-Create Account\n 2. lg-Login\n 3. ex-Exit")
+        shortCode = input().lower()
+
+        if shortCode == 'cc':
+            print("New Account")
+            print("*"*40)
+            print("Username")
+            username = input()
+
+            while True:
+                print(
+                    "1. cp - create own password\n 2. sgp - System generated password")
+                passwordOption = input()
+
+                if passwordOption == 'cp':
+                    print("Your Password")
+                    password = input()
+                    break
+                elif passwordOption == 'sgp':
+                    password = Credentials.passwordGenerate()
+                    break
+
+                else:
+                    print("PasswordOption Invalid")
+
+            createUser = User.createUser(username, password)
+            User.saveUser(createUser)
+            print("\n")
+            print(
+                f"Hi {username} You've sucessfully created you account\n")
+            print(f"Your username is {username} and password is {password}\n")
+
+        elif shortCode == 'lg':
+            print("*"*60)
+            print("Enter your username and password")
+            print("*"*60)
+
+            print("Username")
+            username = input()
+            print("Password")
+            password = input()
+
+            for user in User.userList:
+                if username == user.username:
+                    if user.password == password:
+                        print(user.login())
+                    else:
+                        print("password invalid")
+                        break
+                else:
+                    print("username Invalid")
+                    break
+
+            break
+
+        elif shortCode == 'ex':
+
+            print("Bye. Nice time.")
+            break
+
+        else:
+            print("Invalid shortcode\n")
